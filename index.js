cf6a6396427e44e91594e4151b79f08d00b33eb8
SahatMarisi <sahat346@gmail.com>
2022-10-12 21:29:22 +0700

    update daftar film

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const route = require("./src/routes")
 const app  = express();
 app.use(route)
 app.get('/', (req, res) => {
-    res.send("Hello Word");
+    res.send("backend movtix");
     });
-    app.listen(3000, () => console.log("server running at http://localhost:3000"));
+    app.listen(4000, () => console.log("server running at http://localhost:4000"));
